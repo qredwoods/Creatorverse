78aa96f0a75970746a01df755992a501d905fb49
Quinn Redwoods <quinnredwoods@gmail.com>
2026-02-08 19:14:48 -0800

    connect to db and display top-data on Home.jsx successful

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import ViewCreator from "./pages/ViewCreator";
 import CreatorSettings from "./pages/CreatorSettings";
+import { supabase } from "./client";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [creators, setCreators] = useState([]);
+
+  useEffect(() => {
+    async function fetchCreators() {
+      const { data, error } = await supabase
+        .from("creators")
+        .select("*");
+
+      if (error) {
+        console.error(error);
+      } else {
+        setCreators(data);
+      }
+    }
+
+    fetchCreators();
+  }, []);
+
   const routes = useRoutes([
     {
       path: "/",
       element: <Layout />,
       children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <Home creators={creators}/> },
       { path: "/creators/new", element: <CreatorSettings /> },
       { path: "/creators/:id", element: <ViewCreator /> },
       { path: "/creators/:id/edit", element: <CreatorSettings /> },
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import CreatorCard from "../components/CreatorCard";
 
-export default function Home() {
-  const creator = {
-    name: "Jane Developer",
-    url: "https://janedev.dev",
-    description: "Fullstack engineer who loves React and APIs.",
-    imageURL: "https://se-phd.s3d.cmu.edu/images/people/students/hseih-jane.jpg",
-  };
-
+export default function Home({ creators }) {
   return (
     <div>
       <h1>All Creators</h1>
-      <CreatorCard creator={creator} />
+
+      {creators.length === 0 ? (
+        <p>No content creators found.</p>
+      ) : (
+        creators.map((creator) => (
+          <CreatorCard key={creator.id} creator={creator} />
+        ))
+      )}
     </div>
   );
 }
+
